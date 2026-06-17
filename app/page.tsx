ae57e2e6fe864b61f279cf870b6ced38f146f629
Farah Zamir <dynamicsofanaisteroid@gmail.com>
2026-06-17 19:01:57 +0100

    added star

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
       {/* Intro */}
       <div className="max-w-4xl mx-auto px-6 py-10">
-        <h2 className="text-2xl font-semibold tracking-tight mb-6">Hey, I’m Farah 👋</h2>
+        <h2 className="text-2xl font-semibold tracking-tight mb-6">Hey, I’m Farah ✮⋆˙</h2>
         
         <div className="max-w-3xl space-y-5 text-[20.5px] leading-relaxed text-slate-700">
           <p>
